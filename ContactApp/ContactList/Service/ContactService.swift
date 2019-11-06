7e0f31c88ee376fc12f2aa0ba1b91cf29d1fd5a5
Ridwan Arvihafiz <ridwanarvi@gmail.com>
2019-11-06 19:57:23 +0700

    make contact service

---
+++ ContactApp/ContactList/Service/ContactService.swift
@@ -0,0 +1,52 @@
+//
+//  ContactService.swift
+//  ContactApp
+//
+//  Created by Ridwan Arvihafiz Bakri on 06/11/19.
+//  Copyright © 2019 Ridho Pratama. All rights reserved.
+//
+
+import Foundation
+
+enum ContactServiceError: Error {
+    case missingData
+}
+
+protocol ContactService {
+    func fetchContactList(completion: @escaping (Result<[Contact], Error>) -> Void)
+}
+
+class NetworkContactService: ContactService {
+    private let jsonDecoder: JSONDecoder = {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        return decoder
+    }()
+
+    func fetchContactList(completion: @escaping (Result<[Contact], Error>) -> Void) {
+        let url: URL = URL(string:
+            "https://gist.githubusercontent.com/99ridho/cbbeae1fa014522151e45a766492233c/raw/e3ea7cf52a7de7872863f9b2350f2c434eb0fe2c/contacts.json"
+            )!
+        let task = URLSession.shared.dataTask(with: url) { [jsonDecoder] data, response, error in
+           if let theError = error {
+               completion(.failure(theError))
+               return
+           }
+
+            guard let theData = data else {
+                completion(.failure(ContactServiceError.missingData))
+                return
+            }
+            
+            do {
+                let response = try jsonDecoder.decode(ContactListResponse.self, from: theData)
+                let contacts = response.data
+                completion(.success(contacts))
+            } catch (let decodeError) {
+                completion(.failure(decodeError))
+            }
+        }
+        task.resume()
+    }
+}
